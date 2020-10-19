e09c34bead7e6d56d0c4ff4769777be863e136bd
Ivan Ristović <ivo.ristovic@gmail.com>
2020-10-19 20:30:20 +0200

    Add project files.

---
+++ SpaceSharp/MainWindow.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SpaceSharp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrTick = new System.Windows.Forms.Timer(this.components);
+            this.tmrComet = new System.Windows.Forms.Timer(this.components);
+            this.tmrSupernova = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrTick
+            // 
+            this.tmrTick.Enabled = true;
+            this.tmrTick.Interval = 20;
+            this.tmrTick.Tick += new System.EventHandler(this.tmrTick_Tick);
+            // 
+            // tmrComet
+            // 
+            this.tmrComet.Enabled = true;
+            this.tmrComet.Interval = 3000;
+            this.tmrComet.Tick += new System.EventHandler(this.tmrComet_Tick);
+            // 
+            // tmrSupernova
+            // 
+            this.tmrSupernova.Enabled = true;
+            this.tmrSupernova.Interval = 1500;
+            this.tmrSupernova.Tick += new System.EventHandler(this.tmrSupernova_Tick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainWindow";
+            this.Text = "mmm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainWindow_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmrTick;
+        private System.Windows.Forms.Timer tmrComet;
+        private System.Windows.Forms.Timer tmrSupernova;
+    }
+}
+
---
+++ SpaceSharp/MainWindow.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SpaceSharp
+{
+    public partial class MainWindow : Form
+    {
+        private readonly List<Star> stars = new List<Star>();
+        private readonly List<Comet> comets = new List<Comet>();
+        private IEnumerable<SpaceObject> AllObjects => this.stars.Cast<SpaceObject>().Concat(this.comets);
+
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            var rng = new Random();
+            int starsCount = rng.Next(100, 200);
+            this.stars = Enumerable.Repeat(1, starsCount)
+                                   .Select(_ => new Star {
+                                       X = rng.Next(this.Width),
+                                       Y = rng.Next(this.Height),
+                                       Size = rng.Next(2, 5)
+                                   })
+                                   .ToList()
+                                   ;
+        }
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void tmrTick_Tick(object sender, EventArgs e)
+        {
+            foreach (SpaceObject obj in this.stars.Cast<SpaceObject>().Concat(this.comets))
+                obj.Move();
+            this.Invalidate();
+        }
+
+        private void MainWindow_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            foreach (SpaceObject obj in this.AllObjects)
+                obj.Draw(e.Graphics);
+
+            this.comets.RemoveAll(comet => comet.X < -100 || comet.Y < -100);
+        }
+
+        private void tmrComet_Tick(object sender, EventArgs e)
+        {
+            var rng = new Random();
+            this.comets.Add(new Comet {
+                X = rng.Next(this.Width / 2, this.Width),
+                Y = 0,
+                Size = rng.Next(50, 100),
+                Speed = rng.Next(400, 800),
+                Thickness = (float)rng.NextDouble() + 1f,
+            });
+            this.tmrComet.Interval = rng.Next(3, 5) * 1000;
+        }
+
+        private void tmrSupernova_Tick(object sender, EventArgs e)
+        {
+            this.stars[new Random().Next(stars.Count)].IsSupernova = true;
+        }
+    }
+}
